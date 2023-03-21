e680eb2318b0329e636ef6e0567ccb13c5b1ddff
Brady Pratt <bpratt@redhat.com>
2023-03-21 09:41:36 -0500

    common/util: drop EOL OCP version constraints
    drop end of life openshift version constraints that are no longer
    relevant
    
    /kind cleanup
    
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

--- pkg/common/util/versionutil.go
+++ pkg/common/util/versionutil.go
@@ -12,15 +12,6 @@ import (
 var (
 	// NoVersionFound when no version can be found.
 	NoVersionFound = "NoVersionFound"
-
-	// Version420 represents Openshift version 4.2.0 and above
-	Version420 *semver.Constraints
-
-	// Version440 represents Openshift version 4.4.0 and above
-	Version440 *semver.Constraints
-
-	// Version460 represents Openshift version 4.6.0 and above
-	Version460 *semver.Constraints
 )
 
 // ClusterVersionProvider is a type that can return cluster version
@@ -47,21 +38,5 @@ func OnSupportedVersionIt(constraints *semver.Constraints, helper ClusterVersion
 }
 
 func init() {
-	var err error
-
-	Version420, err = semver.NewConstraint(">= 4.2.0-0")
-	if err != nil {
-		panic(err)
-	}
-
-	Version440, err = semver.NewConstraint(">= 4.4.0-0")
-
-	if err != nil {
-		panic(err)
-	}
-
-	Version460, err = semver.NewConstraint(">= 4.6.0-0")
-	if err != nil {
-		panic(err)
-	}
+	// register new Versions here
 }
